760acac0234daaf5b2e18cd69235f32117e4eaac
Nick Allen <nwestallen@gmail.com>
2021-02-08 15:57:46 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      toDo: [{task: 'Organize Garage', id: 1234, completed: false}]
+    };
+  }
+
+  addTask = (e) => {
+    addItem = e.target
+    this.setState({
+      ...this,
+      toDo: [...toDo, {addItem}]
+    })
+  }
   render() {
     return (
       <div>
---
+++ src/components/TodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+
+render() {
+    return(
+        <form>
+            <label>
+                To Do
+                <input type='text' />
+            </label>
+            
+        </form>
+    )
+}
+}
